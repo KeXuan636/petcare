0cefee8bb85e1ae8c631bbcb260ffc03eb94a3e5
KeXuan636 <kexuanlee0709@gmail.com>
2025-07-30 23:07:01 +0800

    Update pet_care_db.sql

--- database/pet_care_db.sql
+++ database/pet_care_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    contact VARCHAR(20) NOT NULL,
+    contact INT(20) NOT NULL,
     role VARCHAR(20) NOT NULL
 );
 
